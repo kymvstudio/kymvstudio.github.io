be3264554a31311e291daa9ad9e605919920ffc4
kinio-1998 <li.carlosduarte98@gmail.com>
2024-10-03 16:05:16 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://kymvstudio.github.io/",
+  base: "https://kymvstudio.github.io/src/main.jsx",
 })
